18442458090e159b7b80a69f4f69ace9aa15c424
stellar-tarun <stellartarun@gmail.com>
2026-03-10 20:50:14 +0530

    added sql and sql lite code and also the logging code

---
+++ AI_ML/7-Logging/logs/logger.py
@@ -0,0 +1,12 @@
+import logging
+
+## configure logging
+
+logging.basicConfig(
+    filename='app.log',
+    filemode='w',
+    level=logging.DEBUG,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
